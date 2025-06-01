908f31f406422aa2254ea2010a249523bbae6b53
bellandkham9 <bellandkham9@gmaol.com>
2025-06-01 14:21:56 +0100

    five2  commit

--- plante_detect/plante_detect/settings.py
+++ plante_detect/plante_detect/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(rjk*iy3kg-_svx#2-jgvbp10krw=x(87po%vmz8ts2z8qg=eh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
